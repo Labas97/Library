0918e15159801663826abd79a9e869658e93bef3
Labas Tibor Bence <labas1@iit.uni-miskolc.hu>
2018-04-12 08:06:48 +0200

    initial commit

---
+++ src/bookstyle/BookStyle.java
@@ -0,0 +1,10 @@
+package bookstyle;
+
+public class BookStyle extends mybooks.Book.Book{
+
+	String style;
+		
+	public BookStyleString writerName, String bookName, int yearOfRelease, int
+		super
+
+}
